b8ab07f6973f4ed62011b5242dd822e8307f29a6
JaysonVillanuz <114981647+JaysonVillanuz@users.noreply.github.com>
2023-07-05 23:02:42 -0700

    Add files via upload

---
+++ MP2.py
@@ -0,0 +1,10 @@
+num1 = 5
+num2 = 8
+sum = num1 + num2
+diff = num1 - num2
+pro = num1 * num2
+quo = num1 / num2
+print("The sum is ",sum)
+print("The difference is ",diff)
+print("The product is ",pro)
+print("The quotient is ",quo)
