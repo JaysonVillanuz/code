b314cc5a951e11ff27b37abff71f6ae480faac32
JaysonVillanuz <114981647+JaysonVillanuz@users.noreply.github.com>
2023-08-31 15:46:47 +0800

    Create sqlconnector.py

---
+++ sqlconnector.py
@@ -0,0 +1,68 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+     host="localhost",
+     user="root",
+     password="",
+     database="dbstore"
+)
+mycursor = mydb.cursor()
+
+def insert_user():
+     ORNo = input("Enter OR No: ")
+     Amount = input("Enter Amount: ")
+     status = 1
+     sql = "INSERT INTO tblusers (ORNo, Amount) VALUES (%s, %s)"
+     val = (ORNo, Amount)
+     mycursor.execute(sql, val)
+     mydb.commit()
+     print(mycursor.rowcount, "record inserted.")
+
+def delete_user():
+
+     ORNo= input("Enter ORNo: ")
+     sql = "DELETE FROM tblusers WHERE ORNo = '"+ORNo+"'"
+     mycursor.execute(sql)
+     mydb.commit()
+     print(mycursor.rowcount, "record(s) deleted")
+
+def update_user():
+     ORNo = input("Enter the User ID: ")
+     Amount = input("Enter the User Password: ")
+     sql = "UPDATE tblusers SET Amount = '"+Amount+"' WHERE ORNo = '"+ORNo+"'"
+     mycursor.execute(sql)
+     mydb.commit()
+     print(mycursor.rowcount, "record(s) affected")
+
+def search_user():
+     ORNo = input("Enter the User ID: ")
+     sql = "SELECT * FROM tblusers WHERE ORNo='"+ORNo+"'"
+     mycursor.execute(sql)
+     myresult = mycursor.fetchall()
+     for x in myresult:
+         print(x)
+
+def main():
+    while True:
+         print("User Management Menu")
+         print("1. Search User")
+         print("2. Add User")
+         print("3. Update User")
+         print("4. Delete User")
+         print("5. Exit")
+         choice = input("Enter your choice [1/2/3/4/5]: ")
+
+         if choice == '1':
+             search_user()
+         if choice == '2':
+             insert_user()
+         if choice == '3':
+             update_user()
+         if choice == '4':
+             delete_user()
+         if choice == '5':
+              exit()
+              print("Thank you for using User Management System")
+
+if __name__=="__main__":
+     main()
